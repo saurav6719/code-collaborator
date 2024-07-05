40e5471c995d3370b02a3cc8f153a2dcdae67de0
Saurav Kumar <sauravkr485@gmail.com>
2024-07-06 04:21:41 +0530

    project

--- realtime-editor/src/App.js
+++ realtime-editor/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Toaster} from 'react-hot-toast'
 import EditorPage from './pages/editorPage';
 import Home from './pages/home';
 
 function App() {
   return (
     <>
+    <div>
+      <Toaster>
+        position = "top-right" 
+        toastOptions={{
+          success: {
+            theme : {
+              primary: '#4aed88',
+            },
+          },
+        }}
+      </Toaster>
+    </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- realtime-editor/src/pages/home.js
+++ realtime-editor/src/pages/home.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+import {v4 as uuidv4} from 'uuid'
+import toast from 'react-hot-toast'
 
 const Home = () => {
+
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidv4();
+        setRoomId(id);
+        toast.success('Created a New Room');
+    }
     return (
         <div className="homePageWrapper">
             <div className="formWrapper">
                 <img className="homePageLogo" src = "/code-sync-logo.png" alt="code-sync-logo" />
                 <h4 className="mainLabel">Paste Invitation Room ID</h4>
                 <div className="inputGroup">
-                    <input type="text" className="inputBox" placeholder="ROOM ID" />
-                    <input type="text" className="inputBox" placeholder="USER NAME" />
+                    <input type="text" className="inputBox" placeholder="ROOM ID" onChange= {(e) => setRoomId(e.target.value)} value = {roomId}/>
+                    <input type="text" className="inputBox" placeholder="USER NAME" onChange= {(e) => setUsername(e.target.value)} value = {username} />
                     <button className="btn joinBtn">Join</button>
                     <span className="createInfo">
                         If you don't have an invite then create &nbsp;
-                        <a href="" className="createNewBtn">
+                        <a onClick = {createNewRoom} href="" className="createNewBtn">
                             new room
                         </a>
                     </span>
