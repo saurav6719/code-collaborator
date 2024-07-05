fb1cb4d240bc913e11bab65d2837c4a3dbe017c2
Saurav Kumar <sauravkr485@gmail.com>
2024-07-05 22:25:42 +0530

    project starts

---
+++ realtime-editor/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EditorPage from './pages/editorPage';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor" element={<EditorPage />} />
+          <Route path="/editor/:roomId" element={<EditorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
